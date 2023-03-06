1bd23700a4d342d0eee2caa887c26bdade76bdb1
Crowdbotics <engineering@crowdbotics.com>
2023-03-06 00:17:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-water-dev-67938",
+  url: "https://soft-water-dev-67938.botics.co",
+  api: "https://soft-water-dev-67938.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
